1b7020b6b75763c15e5d7a82f1913b4c0ff85bd4
ivanfirmandar <ivanfirmandar@gmail.com>
2020-09-22 13:00:55 +0700

    nambah put get

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,4 +3,8 @@ module.exports = function (app) {
     var todoList = require('../controllers/controller');
     app.get('/tasks', todoList.listAllTasks);
     app.post('/tasks', todoList.create_a_task);
+    app.get('/tasks/:taskid', todoList.read_a_task);
+    app.put('/tasks/:taskid', todoList.update_a_task);
+    app.put('/tasks/:taskid', todoList.delete_a_task);
+
 }
