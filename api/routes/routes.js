c330114794a347a4bfa1b8f82d7060692a472b0c
ivanfirmandar <ivanfirmandar@gmail.com>
2020-09-22 13:01:25 +0700

    nambah delete

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,6 +5,6 @@ module.exports = function (app) {
     app.post('/tasks', todoList.create_a_task);
     app.get('/tasks/:taskid', todoList.read_a_task);
     app.put('/tasks/:taskid', todoList.update_a_task);
-    app.put('/tasks/:taskid', todoList.delete_a_task);
+    app.delete('/tasks/:taskid', todoList.delete_a_task);
 
 }
